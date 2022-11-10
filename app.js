b9c7c90beaacd72f1c839f6577596ddf8f303927
Michael <michaelgandolfi7@gmail.com>
2022-11-10 08:26:17 -0600

    added edit functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOveride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
@@ -23,13 +24,22 @@ const app = express()
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
+// Method override
+app.use(methodOveride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
 // Handlebars Helpers
-const {formatDate, stripTags, truncate} = require('./helpers/hbs')
+const {formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
 
 // Handlebars
 app.engine( 
@@ -38,6 +48,8 @@ app.engine(
             formatDate, 
             stripTags,
             truncate,
+            editIcon,
+            select,
         },
     defaultLayout: 'main',
     extname: '.hbs'
@@ -58,6 +70,12 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global variable
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
